bcc4745065bb09f24fd500e83900b52444a4d5ff
Brice <fromm.brice@gmail.com>
2024-06-06 10:15:37 +0200

    created basic routes

--- server/index.js
+++ server/index.js
@@ -4,10 +4,24 @@ const Sequelize = require('sequelize');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 const router = express.Router();
 
 router.get('/entries', (req, res) => {
   res.send('hello from the router');
+});
+
+router.post('/entries', (req, res) => {
+  res.send('Got a POST request');
+});
+
+// to modify an entry
+router.put('/entries/:id', (req, res) => {
+  res.send('Modified the entry');
+});
+
+router.delete('/entries/:id', (req, res) => {
+  res.send('Deleted the entry');
 });
 
 app.use(router);
