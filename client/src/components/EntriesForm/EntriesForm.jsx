41f7db12e36807a3804fa05578637bb0f6622f2b
Brice <fromm.brice@gmail.com>
2024-06-07 13:32:20 +0200

    debugged select list in EntriesForm

--- client/src/components/EntriesForm/EntriesForm.jsx
+++ client/src/components/EntriesForm/EntriesForm.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
-
+import { postEntry } from '../../apiService';
 function EntriesForm() {
   const [item, setItem] = useState('');
+  const [selectedOption, setSelectedOption] = useState('food');
 
   const handleChange = (event) => {
     setItem(event.target.value);
   };
 
-  const handleSubmit = (event) => {};
-  console.log(item);
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const entry = { name: item, select: selectedOption }; // Create the entry object
+    console.log('Submitting Entry:', entry); // Log the entry before sending
+    postEntry(entry).then(console.log).catch(console.error);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -19,6 +29,15 @@ function EntriesForm() {
           onChange={handleChange}
           placeholder='What did you consume?'
         />
+        <div>
+          <label> Select an option</label>
+          <select value={selectedOption} onChange={handleDropdownChange}>
+            <option value='Food'>Food</option>
+            <option value='Beverage'>Beverage</option>
+            <option value='Medication'>Medication</option>
+            <option value='Supplement'>Supplement</option>
+          </select>
+        </div>
         <button type='submit'>Add</button>
       </form>
     </div>
