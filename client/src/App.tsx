68a712a5bf9d83f83155073f20f63a99afe67653
wyna <queralt.guillen@gmail.com>
2024-06-17 19:32:20 +0200

    EntriesForm + App in tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getEntries } from './apiService';
+import EntriesContext from './context/EntriesContext';
 import EntriesForm from './components/EntriesForm/EntriesForm';
 import EntriesList from './components/EntriesList/EntriesList';
-import EntriesContext from './context/EntriesContext';
 import Header from './components/Header/Header';
-import { Box, Container } from '@mui/material';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MyLists from './components/pages/MyLists/MyLists';
 import SignIn from './components/pages/LoginPage/LoginPage';
-
+import { Container, Box } from '../node_modules/@mui/material/index';
+import { BrowserRouter as Router, Route, Routes } from '../node_modules/react-router-dom/dist/index';
 
 function App() {
-  const [entriesList, setEntriesList] = useState([]);
+  const [entriesList, setEntriesList] = useState<object[]>([]);
 
   //TODO userId is hardcoded
   const userId = 8;
@@ -20,7 +19,7 @@ function App() {
   useEffect(() => {
     getEntries(userId).then((data) =>
       // isEditing: false --> all entries start in view mode (not editable)
-      setEntriesList(data.map((entry) => ({ ...entry, isEditing: false })))
+      setEntriesList(data.map((entry: { id: number; createdAt: string; name: string; select: string; health_impact: string; stool_type: string; is_bleeding: boolean; other_symptoms: string; userId: number; itemId: number }) => ({ ...entry, isEditing: false })))
     );
   }, []);
 
@@ -36,14 +35,7 @@ function App() {
             path='/dashboard'
             element={
               <Container>
-                <Box
-                  display='flex'
-                  flexDirection='column'
-                  alignItems='center'
-                  justifyContent='center'
-                  minHeight='100vh'
-                  padding={2}
-                >
+                <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' minHeight='100vh' padding={2}>
                   <EntriesForm />
                   <EntriesList />
                 </Box>
---
+++ client/src/components/EntriesForm/EntriesForm.tsx
@@ -0,0 +1,111 @@
+import { useContext, useState } from 'react';
+import { Button, Box, Paper, TextField, FormControl, InputLabel, Select, MenuItem, FormGroup, FormControlLabel, Checkbox, SelectChangeEvent } from '../../../node_modules/@mui/material/index';
+
+import { postEntry } from '../../apiService';
+import './style.css';
+import EntriesContext from '../../context/EntriesContext';
+import * as React from 'react';
+
+function EntriesForm() {
+  const { setEntriesList, userId } = useContext(EntriesContext);
+
+  const [formData, setFormData] = useState<{ item: string; selectedOption: string; otherSymptoms: string; selectedStoolType: string; bloodInStool: boolean }>({
+    item: '',
+    selectedOption: '',
+    otherSymptoms: '',
+    selectedStoolType: '',
+    bloodInStool: false,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent<string | EventTarget>) => {
+    let { name, value } = event.target;
+
+    if (name === 'bloodInStool') {
+      value = event.target.value; //had to replace .checked with .value, not sure if it will work in the browser
+    }
+
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!formData.item && !formData.otherSymptoms && !formData.selectedStoolType) {
+      alert('Please fill either the item and select option or other symptoms and stool type.');
+      return;
+    }
+
+    const newItem: { name: string; select: string; other_symptoms: string; stool_type: string; is_bleeding: boolean; userId: number } = {
+      name: formData.item,
+      select: formData.selectedOption,
+      other_symptoms: formData.otherSymptoms,
+      stool_type: formData.selectedStoolType,
+      is_bleeding: formData.bloodInStool,
+      userId: userId,
+    };
+
+    postEntry(newItem).then((newEntry: { name: string; select: string; other_symptoms: string; stool_type: string; is_bleeding: boolean; userId: number }) => {
+      setEntriesList((prevList: [{}]) => [...prevList, { ...newEntry, isEditing: false }]);
+      setFormData({
+        item: '',
+        selectedOption: '',
+        otherSymptoms: '',
+        selectedStoolType: '',
+        bloodInStool: false,
+      });
+    });
+  };
+
+  const itemOptions = ['Food', 'Beverage', 'Medication', 'Supplement'];
+  const stoolTypeOptions = ['Type 1', 'Type 2', 'Type 3', 'Type 4', 'Type 5', 'Type 6', 'Type 7'];
+
+  return (
+    <Paper className='form-container' elevation={10}>
+      <Box className='form-box' component='form' onSubmit={handleSubmit}>
+        <TextField className='form-field' type='text' name='item' value={formData.item} onChange={handleChange} placeholder='What did you consume?' variant='outlined' margin='normal' />
+        <FormControl className='form-field' variant='outlined' margin='normal'>
+          <InputLabel>Select an option</InputLabel>
+          <Select name='selectedOption' value={formData.selectedOption} onChange={handleChange} label='Select an option'>
+            {itemOptions.map((item, index) => (
+              <MenuItem key={index} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <TextField className='form-field' type='text' name='otherSymptoms' value={formData.otherSymptoms} onChange={handleChange} placeholder='What are your symptoms?' variant='outlined' margin='normal' />
+        <FormControl className='form-field' variant='outlined' margin='normal'>
+          <InputLabel>Bristol Stool Scale</InputLabel>
+          <Select name='selectedStoolType' value={formData.selectedStoolType} onChange={handleChange} label='Bristol Stool Scale'>
+            {stoolTypeOptions.map((item, index) => (
+              <MenuItem key={index} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        {formData.selectedStoolType && (
+          <FormGroup>
+            <FormControlLabel control={<Checkbox value={formData.bloodInStool} onChange={handleChange} name='bloodInStool' />} label='Blood in Stool' />
+          </FormGroup>
+        )}
+        <Button
+          className='add-btn'
+          type='submit'
+          variant='contained'
+          color='primary'
+          sx={{
+            width: { xs: '100%', sm: 'auto', md: 'auto' },
+            py: '10px',
+            px: '80px',
+          }}
+        >
+          Add
+        </Button>
+      </Box>
+    </Paper>
+  );
+}
+
+export default EntriesForm;
