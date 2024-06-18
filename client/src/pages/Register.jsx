472a190a2456ba62549e56ddb04b59d919de7403
Sebastian Delgado von Euw <voneuwdel@gmail.com>
2024-06-18 11:16:47 +0200

    register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,32 +1,28 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme';
 import { useNavigate } from 'react-router-dom';
-import { login } from '../services/authService';
+import { register } from '../services/authService';
 import EntriesContext from '../context/EntriesContext';
+import { useContext } from 'react';
 
 export default function Register() {
-  const { setUserId } = React.useContext(EntriesContext);
-
+  const { setUserId } = useContext(EntriesContext);
+  
   const navigate = useNavigate();
 
   async function handleSubmit (event) {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const res = await login(data.get('email'), data.get('password'));
+    const res = await register(data.get('email'), data.get('password'), data.get('firstName'), data.get('lastName'));
     const resData = await res.json();
-    if (res.status === 200) {
+    if (res.status === 201) {
       setUserId(resData.userId);
       return navigate('/dashboard');
     } else {
@@ -76,17 +72,8 @@ export default function Register() {
             alignItems: 'center',
           }}
         >
-          <Avatar
-            sx={{ m: 1, bgcolor: 'secondary.main', width: 120, height: 120 }}
-          >
-            <Avatar
-              alt='Mr Beast'
-              src='/profile-picture.png'
-              sx={{ width: 120, height: 120 }}
-            />
-          </Avatar>
           <Typography component='h1' variant='h5'>
-            Sign in
+            Register
           </Typography>
           <Box
             component='form'
@@ -94,6 +81,24 @@ export default function Register() {
             noValidate
             sx={{ mt: 1 }}
           >
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='firstName'
+              label='First Name'
+              name='firstName'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='lastName'
+              label='Last Name'
+              name='lastName'
+              autoFocus
+            />
             <TextField
               margin='normal'
               required
@@ -112,11 +117,6 @@ export default function Register() {
               label='Password'
               type='password'
               id='password'
-              autoComplete='current-password'
-            />
-            <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Remember me'
             />
             <Button
               type='submit'
@@ -124,20 +124,8 @@ export default function Register() {
               variant='contained'
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              Register me
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href='#' variant='body2' to={'/my-lists'}>
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='#' variant='body2'>
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -23,7 +23,7 @@ async function login (req, res) {
       sameSite: 'lax',
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     });
-    res.status(200).send(JSON.stringify({ userId: user.id }));
+    res.status(200).json({ userId: user.id });
   }
   catch (error) {
     console.error(error);
@@ -44,7 +44,7 @@ async function auth (req, res) {
     
     if (!user) return res.status(401).json({ error: 'Unauthorized' });
     
-    res.status(200).send(JSON.stringify({ userId: user.id }));
+    res.status(200).json({ userId: user.id });
   }
   catch (error) {
     console.error(error);
@@ -57,7 +57,7 @@ async function register (req, res) {
     const { email, password, firstName, lastName } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({ email, password: hashedPassword, firstName, lastName });
-    res.status(201).json({ message: 'Registration successful' });
+    res.status(201).json({ userId: newUser.id });
   }
   catch (error) {
     console.error(error);
