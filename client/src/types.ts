dbd6fe67f0c28f79bbc13538da3cb9eb79611c6f
wyna <queralt.guillen@gmail.com>
2024-06-18 15:58:53 +0200

    TS to apiService file

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,3 +1,7 @@
+import { FC } from 'react';
+
+//Props to pass down to specific components
+
 export interface EntryProps {
   name: string;
   select: string;
@@ -14,4 +18,38 @@ export interface SymptomsEntryProps {
   stool_type: string;
   is_bleeding: string;
   other_symptoms: string;
+}
+
+export interface ConsumedItemEntryProps {
+  itemEntry: {
+    name: string;
+    select: string;
+    health_impact: string;
+  };
+  setItemEntry: FC;
+  isEditing: boolean;
+}
+
+//Types to use in all files that need them
+
+export interface EntryToDataBase {
+  name: string;
+  select: string;
+  other_symptoms: string;
+  stool_type: string;
+  is_bleeding: boolean;
+  userId: number;
+}
+
+export interface EntryFromDataBase {
+  id: number;
+  createdAt: string;
+  name: string;
+  select: string;
+  health_impact: string;
+  stool_type: string;
+  is_bleeding: boolean;
+  other_symptoms: string;
+  userId: number;
+  itemId: number;
 }
