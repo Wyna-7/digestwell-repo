cfff4b5839af1d9492eae50187e7eb2c377b360d
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-06-18 11:55:04 +0200

    app.jsx to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getEntries } from './services/apiService';
+<<<<<<< HEAD:client/src/App.jsx
 import { auth } from './services/authService';
+=======
+>>>>>>> ec309ba (app.jsx to app.tsx):client/src/App.tsx
 import EntriesForm from './components/EntriesForm/EntriesForm';
 import EntriesList from './components/EntriesList/EntriesList';
 import EntriesContext from './context/EntriesContext';
@@ -10,7 +13,6 @@ import { Box, Container } from '@mui/material';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MyLists from './components/pages/MyLists/MyLists';
 import SignIn from './components/pages/LoginPage/LoginPage';
-
 
 function App() {
   const [userId , setUserId] = useState(null);
