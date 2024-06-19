9b51d04018064d29ff3b2ed436d6b3405ca797eb
Sebastian Delgado von Euw <voneuwdel@gmail.com>
2024-06-19 11:27:56 +0200

    structure

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -14,11 +14,12 @@ beforeAll(async () => {
   await sequelize.authenticate();
 });
 
-beforeEach(async () => {
+beforeAll(async () => {
   await sequelize.sync({ force: true });
 });
 
 afterAll(async () => {
+  await sequelize.sync({ force: true });
   await sequelize.close();
 });
 
@@ -33,4 +34,29 @@ describe('Register new user, login, auth and logout', () => {
     expect(response.body).toBeInstanceOf(Object);
     expect(response.body).toHaveProperty('userId', 1);
   });
+  it('should login, set a cookie with the session and return the userId', async () => {});
+  // login should fail for users that are not registered
+  it('should auth, validate the cookie and return the userId', async () => {});
+  // auth should fail for users that are not logged in
+  it('should logout, clear the cookie and return a message', async () => {});
+});
+
+describe('Symptoms CRUD', () => {
+  // need to login before saving a new symptom
+  it('should save a new symptom', async () => {
+    const response = await request(app)
+      .post('/symptoms')
+      .send({'userId': 1, 'name': 'Headache', 'description': 'Pain in the head', 'severity': 5});
+    expect(response.status).toBe(201);
+  });
+  it('should get all symptoms for a user', async () => {});
+  it('should delete a symptom', async () => {});
+  it('should update a symptom', async () => {});
+});
+
+describe('Items CRUD', () => {
+  it('should save a new item', async () => {});
+  it('should get all items for a user', async () => {});
+  it('should delete an item', async () => {});
+  it('should update an item', async () => {});
 });
