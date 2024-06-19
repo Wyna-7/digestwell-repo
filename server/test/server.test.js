022b6d06407417cb1d83700b36db88f938f2e96d
wyna <queralt.guillen@gmail.com>
2024-06-19 19:50:14 +0200

    created new describe block

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,11 +5,13 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const router = require('../router');
+const { describe } = require('node:test');
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
 
+//Do these two beforeAll need to be separated?
 beforeAll(async () => {
   await sequelize.authenticate();
 });
@@ -18,11 +20,9 @@ beforeAll(async () => {
   await sequelize.sync({ force: true });
   const response = await request(app)
     .post('/register')
-    .send({'email': 'test2@email.com', 'password': '123', 'firstName': 'John', 'lastName': 'Doe'});
-  
-  const loginResponse = await request(app)
-    .post('/login')
-    .send({ email: 'test2@email.com', password: '123' });
+    .send({ email: 'test2@email.com', password: '123', firstName: 'John', lastName: 'Doe' });
+
+  const loginResponse = await request(app).post('/login').send({ email: 'test2@email.com', password: '123' });
 
   authCookie = loginResponse.headers['set-cookie'].pop().split(';')[0];
 });
@@ -31,12 +31,11 @@ afterAll(async () => {
   await sequelize.close();
 });
 
-
 describe('Register new user, login, auth and logout', () => {
   it('should save a new user and return the userId', async () => {
     const response = await request(app)
       .post('/register')
-      .send({'email': 'test@email.com', 'password': '123', 'firstName': 'John', 'lastName': 'Doe'});
+      .send({ email: 'test@email.com', password: '123', firstName: 'John', lastName: 'Doe' });
     expect(response.status).toBe(201);
     expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
     expect(response.body).toBeInstanceOf(Object);
@@ -44,28 +43,38 @@ describe('Register new user, login, auth and logout', () => {
   });
 
   it('should login, set a cookie with the session and return the userId', async () => {
-    const response = await request(app)
-      .post('/login')
-      .send({'email': 'test@email.com', 'password': '123'});
+    const response = await request(app).post('/login').send({ email: 'test@email.com', password: '123' });
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
     expect(response.body).toBeInstanceOf(Object);
+    expect(response.headers['set-cookie'][0].includes('sessionId')).toBe(true);
     expect(response.body).toHaveProperty('userId', 2);
   });
+});
+
+describe('Auth and logout tests', () => {
+  beforeEach(async () => {
+    await request(app).post('/login').send({ email: 'test@email.com', password: '123' });
+  });
   // login should fail for users that are not registered
-  it('should auth, validate the cookie and return the userId', async () => {});
+  it('should auth, validate the cookie and return the userId', async () => {
+    expect(response.headers['set-cookie'][0].includes('sessionId')).toBe(true);
+    const authResponse = await request(app).get('/auth');
+    console.log('--------', response);
+    expect(response.headers['set-cookie'][0].includes('sessionId')).toBe(true);
+    expect(response.body).toHaveProperty('userId', 2);
+  });
   // auth should fail for users that are not logged in
   it('should logout, clear the cookie and return a message', async () => {});
 });
 
 describe('Symptoms CRUD', () => {
-  
   // need to login before saving a new symptom
   it('should save a new symptom', async () => {
     const response = await request(app)
       .post('/symptoms')
       .set('Cookie', authCookie)
-      .send({'userId': 1, 'stool_type': 'Type 1', 'is_bleeding': true, 'other_symptoms': ''});
+      .send({ userId: 1, stool_type: 'Type 1', is_bleeding: true, other_symptoms: '' });
     expect(response.status).toBe(201);
   });
   it('should get all symptoms for a user', async () => {});
